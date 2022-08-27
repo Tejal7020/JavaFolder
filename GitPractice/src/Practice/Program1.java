e663a8ca661e54c8e9ccd04929ae630d50afa6fa
Tejal <tejalgavade1709@gmail.com>
2022-08-27 19:55:18 +0530

    New Project-

---
+++ GitPractice/src/Practice/Program1.java
@@ -0,0 +1,16 @@
+package Practice;
+
+public class Program1 {
+	
+	public static void main(String[] args) {
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
